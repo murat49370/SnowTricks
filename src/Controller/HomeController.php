3d7348b817969a86c464652552af9364540f9a12
Murat49370 <murat49370@gmail.com>
2020-10-29 16:30:37 +0100

    install menu and footer. Create exemple pages.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'request' => $request,
+        ]);
+    }
+}
